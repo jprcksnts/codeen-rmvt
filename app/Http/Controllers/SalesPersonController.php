067c5e3b35c000ffa143a7e76c1284faa671cd3f
Rick <jprck.snts@gmail.com>
2017-11-29 22:23:25 +0800

    Added Models, Controllers, Seeders, and Factories

--- app/Http/Controllers/SalesPersonController.php
+++ app/Http/Controllers/SalesPersonController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class SalesPersonController extends Controller
 {
-    public function getAllSalesPerson()
+    public static function getAllSalesPerson()
     {
         $salesPerson = SalesPerson::all();
         return $salesPerson;
--- resources/views/navigations/message_recipients.blade.php
+++ resources/views/navigations/message_recipients.blade.php
@@ -1,9 +1,9 @@
 <?php
-
 use App\Http\Controllers\MessageController;
-
+use App\Http\Controllers\SalesPersonController;
 ?>
 
-<ul id="message-recipients-nav" class="side-nav fixed">
-
-</ul>
+{{\App\Http\Controllers\SalesPersonController::getAllSalesPerson()}}
+{{--<ul id="message-recipients-nav" class="side-nav fixed">--}}
+    {{----}}
+{{--</ul>--}}
