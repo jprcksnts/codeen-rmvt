f10785e8c22d0974bcb99f5a9976100e07cf75f7
Rick <jprck.snts@gmail.com>
2017-12-02 00:57:45 +0800

    asdf

--- app/Http/Controllers/GoalMetersController.php
+++ app/Http/Controllers/GoalMetersController.php
@@ -6,10 +6,5 @@ use Illuminate\Http\Request;
 
 class GoalMetersController extends Controller
 {
-    protected $fillable = [
-        'id', 'sales_person_id', 'quarter', 'yeaer', 'amount',
-    ];
-    protected $hidden = [
-        'created_at',
-    ];
+
 }
